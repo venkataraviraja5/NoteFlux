5c202642f1c5e243177d83b25b691bf8e2ab793a
PriyeshPandey2000 <priyeshpandey2000@gmail.com>
2024-06-11 16:39:47 +0530

    .

---
+++ src/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,68 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcryptjs";
+import User from "@/models/User";
+import connect from "@/utils/db";
+
+export const authOptions = {
+  providers: [
+    CredentialsProvider({
+      id: "credentials",
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        await connect();
+        try {
+          const user = await User.findOne({ email: credentials.email });
+          if (user) {
+            const isPasswordCorrect = await bcrypt.compare(
+              credentials.password,
+              user.password
+            );
+            if (isPasswordCorrect) {
+              return user;
+            }
+          }
+          return null;
+        } catch (err) {
+          throw new Error(err);
+        }
+      },
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID ?? "",
+      clientSecret: process.env.GOOGLE_SECRET ?? "",
+    }),
+  ],
+  callbacks: {
+    async signIn({ user, account }) {
+      if (account.provider === "credentials") {
+        return true;
+      }
+      if (account.provider === "google") {
+        await connect();
+        try {
+          const existingUser = await User.findOne({ email: user.email });
+          if (!existingUser) {
+            const newUser = new User({
+              email: user.email,
+            });
+            await newUser.save();
+          }
+          return true;
+        } catch (err) {
+          console.log("Error saving user", err);
+          return false;
+        }
+      }
+      return true;
+    },
+  },
+};
+
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
---
+++ src/app/api/register/route.js
@@ -0,0 +1,31 @@
+import User from "@/models/User";
+import connect from "@/utils/db";
+import bcrypt from "bcryptjs";
+import { NextResponse } from "next/server";
+
+export const POST = async (request) => {
+  const { email, password } = await request.json();
+
+  await connect();
+
+  const existingUser = await User.findOne({ email });
+
+  if (existingUser) {
+    return new NextResponse("Email is already in use", { status: 400 });
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 5);
+  const newUser = new User({
+    email,
+    password: hashedPassword,
+  });
+
+  try {
+    await newUser.save();
+    return new NextResponse("User is registered", { status: 200 });
+  } catch (err) {
+    return new NextResponse(err.message || "Internal Server Error", {
+      status: 500,
+    });
+  }
+};
