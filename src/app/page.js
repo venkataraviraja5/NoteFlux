834b3415ba60e14a5f4b9ca821596cea7121c4d0
PriyeshPandey2000 <priyeshpandey2000@gmail.com>
2024-06-13 12:04:20 +0530

    .

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import VoiceText from "../components/VoiceText"
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    <h1>HOMEPAGE</h1>
+    <VoiceText/>
     </main>
   );
 }
