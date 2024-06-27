89dcbfc6cefbac6634921199cc74f2753a2a0d04
PriyeshPandey2000 <priyeshpandey2000@gmail.com>
2024-06-27 22:15:07 +0530

    .

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -2,7 +2,7 @@ import NextAuth from "next-auth";
 import { NEXT_AUTH } from "../../../lib/auth";
 
 
-export const authOptions = NEXT_AUTH
+// export const authOptions = NEXT_AUTH
 
 const handler = NextAuth(NEXT_AUTH);
 export { handler as GET, handler as POST };
