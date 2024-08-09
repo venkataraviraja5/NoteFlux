af8cbf85c3b6c0e624f0caecb05bee4da6e33b87
ravi raja <131418248+venkataraviraja5@users.noreply.github.com>
2024-08-09 09:07:11 +0530

    upload pdf feature

---
+++ src/models/Pdf.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+const PdfSchema = new mongoose.Schema({
+  userId : {
+    type : String,
+    require :true
+  },
+  pdfName : {
+    type : String,
+    require : true
+  },
+  pdfUrl : {
+    type : String,
+    require : true
+  }
+});
+
+export default mongoose.models.Pdf || mongoose.model('Pdf', PdfSchema);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+import { withUt } from "uploadthing/tw";
+ 
+export default withUt({
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -15,4 +17,5 @@ module.exports = {
     },
   },
   plugins: [],
-};
+});
+
